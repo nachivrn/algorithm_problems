1b32fc4fd2371dbf23a9599db42e44670137df2b
nachi <nachiappan_vrn@yahoo.com>
2014-01-04 01:19:12 -0800

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,208 @@
+public class LinkedList {
+
+    static Node buildOneTwoThree() {
+        Node three = new Node(3, null);
+        Node two = new Node(2, three);
+        Node one = new Node(1, two);
+        return one;
+    }
+
+    static Node buildFromIntArr(int[] intArr) {
+        Node head = new Node(intArr[intArr.length - 1], null);
+        for (int i = intArr.length - 2; i >= 0; i--) {
+            Node prev = new Node(intArr[i], head);
+            head = prev;
+        }
+        return head;
+    }
+
+    static int count(Node head) {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+
+    static Node reverse(Node head) {
+        Node pointer1 = head;
+        Node pointer3 = null;
+        Node pointer2;
+
+        while (pointer1 != null) {
+            pointer2 = pointer1.next;
+            pointer1.next = pointer3;
+            pointer3 = pointer1;
+            pointer1 = pointer2;
+        }
+        head = pointer3;
+        return head;
+    }
+
+    static Node recursiveReverse(Node head) {
+        if ((head == null || head.next == null)) {
+            return head;
+        }
+        Node result = recursiveReverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return result;
+    }
+
+    static Node sortedInsert(Node head, Node newNode) {
+        if (head == null || head.getData() > newNode.getData()) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.getData() < newNode.getData()) {
+                    current = current.next;
+                } else {
+                    break;
+                }
+            }
+
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        return head;
+    }
+
+    static Node insertSort(Node head) {
+        Node result = null;
+        Node temp = null;
+        Node current = head;
+        while (current != null) {
+            temp = current;
+            current = current.next;
+            result = sortedInsert(result, temp);
+        }
+        return result;
+    }
+
+    static Node[] frontBackSplit(Node head) {
+        int length = count(head);
+        int midIndex = (length + 1) / 2;
+        Node second = null;
+        Node first = head;
+
+        for (int i = 1; i < midIndex; i++) {
+            first = first.next;
+        }
+        second = first.next;
+        first.next = null;
+        first = head;
+        return new Node[]{first, second};
+    }
+
+    static Node removeDuplicates(Node head) {
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.next.getData() == current.getData()) {
+                current.next = current.next.next;
+            }
+            current = current.next;
+        }
+        return current;
+    }
+
+    static Node[] moveNode(Node dest, Node source) {
+        if (source == null)
+            return null;
+        Node temp = source.next;
+        source.next = dest;
+        dest = source;
+        source = temp;
+        return new Node[]{dest, source};
+    }
+
+    static Node[] alternateSplit(Node head) {
+        int count = 0;
+        Node first = null;
+        Node second = null;
+        Node current = head;
+        Node[] nodeArr = null;
+        while (current != null) {
+            if (count % 2 == 0) {
+                nodeArr = moveNode(second, current);
+                second = nodeArr[0];
+                current = nodeArr[1];
+            } else {
+                nodeArr = moveNode(first, current);
+                first = nodeArr[0];
+                current = nodeArr[1];
+            }
+            count++;
+        }
+        return new Node[]{first, second};
+    }
+
+    static Node shuffleMerge(Node list1, Node list2) {
+        Node result = null;
+        Node[] nodeArr = null;
+        while (list1 != null && list2 != null) {
+            nodeArr = moveNode(result, list1);
+            result = nodeArr[0];
+            list1 = nodeArr[1];
+            nodeArr = moveNode(result, list2);
+            result = nodeArr[0];
+            list2 = nodeArr[1];
+        }
+        while (list1 != null) {
+            Node temp = list1;
+            list1 = list1.next;
+            temp.next = result;
+            result = temp;
+        }
+        while (list2 != null) {
+            Node temp = list2;
+            list2 = list2.next;
+            temp.next = result;
+            result = temp;
+        }
+        return reverse(result);
+    }
+
+    public static void main(String[] args) {
+        Node head = buildOneTwoThree();
+        head = sortedInsert(head, new Node(5));
+        head = sortedInsert(head, new Node(4));
+        head = sortedInsert(head, new Node(0));
+        head = recursiveReverse(head);
+        head = reverse(head);
+        head = insertSort(head);
+        Node[] splitArr = frontBackSplit(head);
+        splitArr = frontBackSplit(new Node(7, new Node(8, new Node(9, null))));
+        Node firstList = buildOneTwoThree();
+        Node secondList = buildOneTwoThree();
+        moveNode(secondList, firstList);
+        head = buildFromIntArr(new int[]{1, 2, 1, 2, 1});
+        alternateSplit(head);
+        Node list1 = buildFromIntArr(new int[]{1, 3, 5, 7});
+        Node list2 = buildFromIntArr(new int[]{2, 4, 6});
+        Node result = shuffleMerge(list1, list2);
+
+    }
+
+    static class Node {
+        Node next;
+        private int data;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node ref) {
+            this.data = data;
+            this.next = ref;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+    }
+}
